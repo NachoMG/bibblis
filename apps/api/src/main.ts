3d042a037e2db580c223b52a95074aee35b6cd76
NachoMG <nacho.m.garcia@gmail.com>
2023-04-16 23:05:38 +0200

    Added Swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,17 +1,22 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
-import { Logger } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = "api";
+  const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Libbuk')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
