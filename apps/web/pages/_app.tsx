13c6565afd4cb73caf43d015831dfaf951c7056f
NachoMG <nacho.m.garcia@gmail.com>
2023-05-04 15:45:41 +0200

    Added Bootstrap

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,13 +1,10 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
-import './styles.css';
+
+import '../styles/main.scss';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Welcome to web!</title>
-      </Head>
       <main className="app">
         <Component {...pageProps} />
       </main>
