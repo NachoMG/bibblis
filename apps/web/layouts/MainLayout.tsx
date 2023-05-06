887093e0fe377e7f4a3d037da1f9dacdb59996e5
NachoMG <nacho.m.garcia@gmail.com>
2023-05-06 13:48:06 +0200

    Removed unnecesary div element in MainLayout component

--- apps/web/layouts/MainLayout.tsx
+++ apps/web/layouts/MainLayout.tsx
@@ -5,13 +5,13 @@ import Footer from '../components/Footer';
 
 const MainLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div>
+    <>
       <Header />
       <main className="container bg-white py-5 px-5">
         { children }
       </main>
       <Footer />
-    </div>
+    </>
   );
 };
 
