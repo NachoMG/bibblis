ee23aca8b818671ceeffeee94e75aaa0e5f77a73
NachoMG <nacho.m.garcia@gmail.com>
2023-05-13 19:13:16 +0200

    Make content fill at least the viewport

--- apps/web/components/Footer.tsx
+++ apps/web/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="row bg-primary px-2 px-md-5 py-4">
+    <footer className="row bg-primary px-2 px-md-5 py-4 mt-auto">
       <div className="col">
         <b>Bibblis 2023</b>
         {' '}| <Link href="/terms-and-conditions">Términos y condiciones</Link>
--- apps/web/layouts/MainLayout.tsx
+++ apps/web/layouts/MainLayout.tsx
@@ -5,13 +5,13 @@ import Footer from '../components/Footer';
 
 const MainLayout = ({ children }: PropsWithChildren) => {
   return (
-    <>
+    <div className="min-vh-100 d-flex flex-column">
       <Header />
-      <main className="container bg-white py-5 px-5">
+      <main className="container bg-white py-5 px-5 my-auto">
         { children }
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
