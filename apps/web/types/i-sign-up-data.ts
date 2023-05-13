45c995fb430edd8a4b07a24533c7a837426ceb8e
NachoMG <nacho.m.garcia@gmail.com>
2023-05-13 19:52:26 +0200

    Fixed identation

--- apps/web/types/i-sign-up-data.ts
+++ apps/web/types/i-sign-up-data.ts
@@ -1,12 +1,12 @@
 export interface ISignUpData {
-    firstName: string;
-    lastName: string;
-    birthDate: Date;
-    email: string;
-    emailConfirm: string;
-    password: string;
-    passwordConfirm: string;
-    termsAndConditions: boolean;
-    acceptedMarketing: boolean;
-    lang?: string;
+  firstName: string;
+  lastName: string;
+  birthDate: Date;
+  email: string;
+  emailConfirm: string;
+  password: string;
+  passwordConfirm: string;
+  termsAndConditions: boolean;
+  acceptedMarketing: boolean;
+  lang?: string;
 }
