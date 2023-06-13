c79d77953fd3ef70e61a1ec85a23efda748c6212
NachoMG <nacho.m.garcia@gmail.com>
2023-06-13 21:05:21 +0200

    Header responsive

--- apps/web/components/Header.tsx
+++ apps/web/components/Header.tsx
@@ -21,42 +21,73 @@ const Header = () => {
   };
 
   return (
-    <header className="row bg-primary px-2 px-md-5 py-2 align-items-center">
-      <div className="col">
-        <Link href="/">
-          <Image src={'/bibblis-logo.svg'} alt="logo" height={50} width={100} />
-        </Link>
+    <header className="bg-primary px-2 px-md-5 py-2">
+      <div className="row align-items-center">
+        <div className="col">
+          <Link href="/">
+            <Image src={'/bibblis-logo.svg'} alt="logo" height={50} width={100} />
+          </Link>
+        </div>
+        <div className="col-7 col-md-4 col-lg-3">
+          <IsbnSearchBox />
+        </div>
+        {isUserLoggedIn &&
+          <>
+            <div className="d-none d-md-block col-auto px-0">
+              <Link href="/my-library" className="btn btn-secondary">
+                Mi librería
+              </Link>
+            </div>
+            <div className="d-none d-md-block col-auto">
+              <a onClick={signOut} className="btn btn-tertiary">
+                Cerrar sesión
+              </a>
+            </div>
+          </>
+        ||
+          <>
+            <div className="d-none d-md-block col-auto px-0">
+              <Link href="/sign-in" className="btn btn-secondary">
+                Iniciar sesión
+              </Link>
+            </div>
+            <div className="d-none d-md-block col-auto">
+              <Link href="/sign-up" className="btn btn-tertiary">
+                Registrarse
+              </Link>
+            </div>
+          </>
+        }
       </div>
-      <div className="col-7 col-md-4 col-lg-3">
-        <IsbnSearchBox />
+      <div className="row d-md-none mt-2">
+        {isUserLoggedIn &&
+          <>
+            <div className="col-6">
+              <Link href="/my-library" className="btn btn-secondary w-100">
+                Mi librería
+              </Link>
+            </div>
+            <div className="col-6">
+              <a onClick={signOut} className="btn btn-tertiary w-100">
+                Cerrar sesión
+              </a>
+            </div>
+          </>
+        ||
+          <>
+            <div className="col-6">
+              <Link href="/sign-in" className="btn btn-secondary w-100">
+                Iniciar sesión
+              </Link>
+            </div>
+            <div className="col-6">
+              <Link href="/sign-up" className="btn btn-tertiary w-100">
+                Registrarse
+              </Link>
+            </div>
+          </>
+        }
       </div>
-      {isUserLoggedIn &&
-        <>
-          <div className="d-none d-md-block col-auto px-0">
-            <Link href="/my-library" className="btn btn-secondary">
-              Mi librería
-            </Link>
-          </div>
-          <div className="d-none d-md-block col-auto">
-            <a onClick={signOut} className="btn btn-tertiary">
-              Cerrar sesión
-            </a>
-          </div>
-        </>
-      ||
-        <>
-          <div className="d-none d-md-block col-auto px-0">
-            <Link href="/sign-in" className="btn btn-secondary">
-              Iniciar sesión
-            </Link>
-          </div>
-          <div className="d-none d-md-block col-auto">
-            <Link href="/sign-up" className="btn btn-tertiary">
-              Registrarse
-            </Link>
-          </div>
-        </>
-      }
     </header>
   );
 };
